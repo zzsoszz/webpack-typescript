00f103614ab8a1ab5d7b49022fdee4e32f3340d5
hyq2015 <18030638805@163.com>
2016-11-22 11:08:45 +0800

    update

---
+++ 1.bundle.js
@@ -0,0 +1,36 @@
+webpackJsonp([1],[
+/* 0 */,
+/* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;!(__WEBPACK_AMD_DEFINE_ARRAY__ = [__webpack_require__, exports, __webpack_require__(2)], __WEBPACK_AMD_DEFINE_RESULT__ = function (require, exports, greet_1) {
+	    "use strict";
+	    function hello(compiler) {
+	        console.log("Hello from " + compiler);
+	    }
+	    hello("TypeScript");
+	    console.log(greet_1.sayHello("TypeScript"));
+	}.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+
+
+/***/ },
+/* 2 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var cbs = [], 
+		data;
+	module.exports = function(cb) {
+		if(cbs) cbs.push(cb);
+		else cb(data);
+	}
+	__webpack_require__.e/* nsure */(2, function(require) {
+		data = __webpack_require__(3);
+		var callbacks = cbs;
+		cbs = null;
+		for(var i = 0, l = callbacks.length; i < l; i++) {
+			callbacks[i](data);
+		}
+	});
+
+/***/ }
+]);
---
+++ main.ts
@@ -0,0 +1,10 @@
+import { sayHello } from "./greet";
+
+
+function hello(compiler: string) {
+    console.log(`Hello from ${compiler}`);
+}
+hello("TypeScript");
+
+
+console.log(sayHello("TypeScript"));
